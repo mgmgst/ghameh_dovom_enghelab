fc938711631da36de2f8e81f7d06ec8316cca57d
mgadmin <mgstudio884@gmail.com>
2020-03-02 18:06:45 +0330

    small change in 404 page

--- app/main.py
+++ app/main.py
@@ -49,6 +49,7 @@ def load_user(userid):
     return User(userid)    
 
 @app.errorhandler(404)
+@login_required
 def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
